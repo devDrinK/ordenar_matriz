e87e8165ee146ab1dcfc919cd0d40dcd94426e4b
devjpunk <91user101@protonmail.com>
2024-11-24 19:51:11 -0600

    primer commit

---
+++ ordenar/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ordenar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.txtMatriz = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtNum
+            // 
+            this.txtNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtNum.Location = new System.Drawing.Point(341, 12);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 26);
+            this.txtNum.TabIndex = 1;
+            this.txtNum.Click += new System.EventHandler(this.txtNum_Click);
+            this.txtNum.TextChanged += new System.EventHandler(this.txtNum_TextChanged);
+            // 
+            // txtMatriz
+            // 
+            this.txtMatriz.Location = new System.Drawing.Point(341, 64);
+            this.txtMatriz.Name = "txtMatriz";
+            this.txtMatriz.Size = new System.Drawing.Size(100, 20);
+            this.txtMatriz.TabIndex = 2;
+            this.txtMatriz.TextChanged += new System.EventHandler(this.txtMatriz_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtMatriz);
+            this.Controls.Add(this.txtNum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.TextBox txtMatriz;
+    }
+}
+
---
+++ ordenar/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ordenar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random x = new Random();
+        int anchoCelda = 30;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtNum.Multiline = false;
+            txtMatriz.Multiline = true;
+            txtMatriz.ScrollBars = ScrollBars.Both;
+            txtMatriz.WordWrap = false;
+            txtMatriz.ReadOnly = true;
+            txtMatriz.Font = new Font("Courier New", 12);
+        }
+
+        float factorAncho = 1.8f; // Puedes ajustar este factor
+        float factorAlto = 1.2f;
+        private async void txtNum_Click(object sender, EventArgs e)
+        {
+            txtMatriz.Clear();
+
+            if (int.TryParse(txtNum.Text, out int def) && def > 0)
+            {
+                int[,] matriz = new int[def, def];
+                int altoCelda = 30;
+                int margen = 20;
+
+                txtMatriz.Width = (int)((def * anchoCelda + margen) * factorAncho);
+                txtMatriz.Height = (int)((def * altoCelda + margen) * factorAlto);
+
+                CenterTextBox();
+
+                // Generar la matriz con valores aleatorios
+                for (int i = 0; i < def; i++)
+                {
+                    for (int j = 0; j < def; j++)
+                    {
+                        matriz[i, j] = x.Next(50);
+                    }
+                }
+
+                // Imprimir la matriz original
+                for (int i = 0; i < def; i++)
+                {
+                    string row = "| ";
+                    for (int j = 0; j < def; j++)
+                    {
+                        row += matriz[i, j].ToString("D2") + " | ";
+                    }
+                    txtMatriz.AppendText(row + Environment.NewLine);
+
+                    if (i < def - 1)
+                    {
+                        string separator = "|";
+                        for (int j = 0; j < def; j++)
+                        {
+                            separator += new string('_', 4) + "|";
+                        }
+                        txtMatriz.AppendText(separator + Environment.NewLine);
+                    }
+
+                    await Task.Delay(500);
+                }
+
+                await Task.Delay(1000);
+
+                // Ordenar la matriz de menor a mayor
+                var orden = matriz.Cast<int>().OrderBy(x => x).ToArray();
+
+                int k = 0;
+                txtMatriz.AppendText(Environment.NewLine + "Matriz Ordenada:" + Environment.NewLine);
+                txtMatriz.Clear();
+
+                // Imprimir la matriz ordenada
+                for (int i = 0; i < def; i++)
+                {
+                    string row = "| ";
+                    for (int j = 0; j < def; j++)
+                    {
+                        row += orden[k++].ToString("D2") + " | ";
+                    }
+                    txtMatriz.AppendText(row + Environment.NewLine);
+
+                    if (i < def - 1)
+                    {
+                        string separator = "|";
+                        for (int j = 0; j < def; j++)
+                        {
+                            separator += new string('_', 4) + "|";
+                        }
+                        txtMatriz.AppendText(separator + Environment.NewLine);
+                    }
+
+                    await Task.Delay(500);
+                }
+            }
+            else if(txtNum.Text == "")
+            { }
+            else
+            {
+                MessageBox.Show("Ingresa solo números enteros positivos.");
+            }
+        }
+
+        private void CenterTextBox()
+        {
+            txtMatriz.Left = (this.ClientSize.Width - txtMatriz.Width) / 2;
+            txtMatriz.Top = (this.ClientSize.Height - txtMatriz.Height) / 2;
+        }
+
+        private void txtNum_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void txtMatriz_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
